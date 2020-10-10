4444af87459ee644b2b3a1e51395696ecb3b48e7
tmeaders <tmeaders@sbcglobal.net>
2020-10-10 14:25:08 -0700

    wk4

--- lesson4/js4/lastmodified4.js
+++ lesson4/js4/lastmodified4.js
@@ -1,9 +1,17 @@
-let date = new Date();
+/*let date = new Date();
 let currentyear = date.getFullYear();
 let lastupdate = document.lastModified;
 document.getElementById("lastupdate").innerHTML = lastupdate;
 document.getElementById("year").innerHTML = currentyear;
+*/
 
-function toggleMenu(){
+try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('lastupdate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
+
+  function toggleMenu(){
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+} 

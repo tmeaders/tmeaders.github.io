8703aa493899bac1b18ea4adee4be487dff460ab
tmeaders <tmeaders@sbcglobal.net>
2020-10-17 16:20:24 -0700

    wk2

--- lesson5/js5/lastmodified5.js
+++ lesson5/js5/lastmodified5.js
@@ -9,24 +9,29 @@ document.getElementById("year").innerHTML = currentyear;
 /*Display the day of the week, month by name, and year*/
 
 try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('lastupdate').textContent = new Date().toLocaleDateString('en-US', options);
-  } catch (e) {
-    alert('Error with code or your browser does not support Locale');
-  }
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  };
+  document.getElementById('lastupdate').textContent = new Date().toLocaleDateString('en-US', options);
+} catch (e) {
+  alert('Error with code or your browser does not support Locale');
+}
 
-  /*Display the responsive hamburger menu button */
-  function toggleMenu(){
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-} 
+/*Display the responsive hamburger menu button */
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
 
 
 /* Saturday pancake banner */
 
 let show = new Date();
 
-if(show.getDay() == 5 ){
-    document.getElementById("sat-pancakes").style.display = "block";
-} else{
-    document.getElementById("sat-pancakes").style.display = "none";
+if (show.getDay() == 5) {
+  document.getElementById("sat-pancakes").style.display = "block";
+} else {
+  document.getElementById("sat-pancakes").style.display = "none";
 }

422cdb003f1aaff3e2d221179b2878a48b37f75f
tmeaders <tmeaders@sbcglobal.net>
2020-11-14 19:21:32 -0800

    wk9

--- lesson9/prophetsjs/prophets.js
+++ lesson9/prophetsjs/prophets.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophttps.json';
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
 .then(function (response) {

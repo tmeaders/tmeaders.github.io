83f3ddd91a755cbc3870129fa981c931dc8e51d0
tmeaders <tmeaders@sbcglobal.net>
2020-11-21 19:41:03 -0800

    wk10

---
+++ lesson10/js10/curtemp.js
@@ -0,0 +1,20 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=466dca7cca93940a33882268fd25bd99';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    
+    let t = parseFloat(jsObject.main.temp);
+    let s = parseFloat(jsObject.wind.speed);
+    let output = "N/A";
+    document.getElementById('weather').innerHTML = jsObject.weather[0].description;
+    document.getElementById('current-temp').innerHTML = Math.round(t) + "&#8457;";
+    if (t <= 50 && s >= 3) {
+      let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
+      output = Math.round(f);
+    };
+    document.getElementById("output").innerHTML = output + "&#8457;";
+    document.getElementById('humidity').innerHTML = jsObject.main.humidity + "&#37;";
+    document.getElementById('speed').innerHTML = Math.round(s) + " mph";
+
+  });
+

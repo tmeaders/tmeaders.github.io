c07dee63061b01c250cf055678cbba4f0966c05f
tmeaders <tmeaders@sbcglobal.net>
2020-11-27 16:38:29 -0800

    wk11

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -56,33 +56,3 @@ if (show.getDay() == 5) {
 function adjustRating(rating) {
   document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-/*  ---------------- lazy load images ------------------------------------  */
-
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-const imgOptions = {
-    threshold: 0.5
-};
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute("data-src");
-    };
-};
-if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
-        items.forEach((item) => {
-            if (item.isIntersecting){
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    },imgOptions);
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    });
-} else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-}

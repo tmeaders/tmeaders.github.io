7df19aaceed8dd0edff8ac15eecf95f4310b0305
tmeaders <tmeaders@sbcglobal.net>
2020-11-28 16:49:09 -0800

    wk11

---
+++ lesson11/js/events-f.js
@@ -0,0 +1,26 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < jsonObject.towns.length; i++) {
+            if (towns[i].name == 'Fish Haven') {
+                let townnames = jsonObject.towns[i].events;
+                townnames.innerHTML = " ";
+                let twnevt = document.createElement('Section');
+                for (i in towns[i].events) {
+                    let eventArray = document.createElement('div')
+                    eventArray.innerHTML = `${townnames[i]}`;
+                    twnevt.setAttribute('class', 'event');
+                    document.querySelector('section.twnevt').appendChild(eventArray);
+                }
+            }
+        }
+
+
+    });
